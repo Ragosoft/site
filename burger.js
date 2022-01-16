377f0ccd9e04b4684c820235c13bdb5b65e4c555
Ragosoft <97634656+Ragosoft@users.noreply.github.com>
2022-01-16 08:29:08 +0100

    New site

---
+++ burger.js
@@ -0,0 +1,18 @@
+let burger = document.querySelector(".burger");
+let navMenu = document.querySelector(".nav__menu");
+
+burger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+	burger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu(){
+	burger.classList.remove("active");
+	navMenu.classList.remove("active");
+}
